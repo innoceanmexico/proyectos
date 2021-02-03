8fb36d7f2c6b27f4c372b5a2d60061b803cef856
Axel <axosar@gmail.com>
2021-02-03 09:58:57 -0600

    first commit

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>INNOCEAN MÉXICO PROYECTOS</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
