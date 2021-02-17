09b7d347691ac5afd22147cfdec2b333f7beb3c5
Axel <axosar@gmail.com>
2021-02-17 11:32:21 -0600

    Kia layout

--- pages/kia.js
+++ pages/kia.js
@@ -14,18 +14,23 @@ function Kia() {
           <Col>
             <h2 className="mt-3">
               <span>
-                <span style={{ color: '#f0582a' }}>[</span> PROYECTO KIA <span style={{ color: '#f0582a' }}>]</span>
+                <span style={{ color: '#f0582a' }}>[</span> PROYECTOS KIA <span style={{ color: '#f0582a' }}>]</span>
               </span>
             </h2>
             <hr />
           </Col>
         </Row>
-        <Row style={{ marginBottom: '90px'}}>
+        <Row>
           <Col>
-            <div align="center">
+          <div align="center">
               <img src="/images/logo-kia.jpg" alt="" alt="" />
             </div>
             <hr/>
+          </Col>
+        </Row>
+        <Row style={{ marginBottom: '90px'}}>
+          <Col xs={6}>
+            
             <ul className="menuLateral">
               <li>
                 <Link href="/cotizacion/kiacareers">
@@ -41,10 +46,21 @@ function Kia() {
                   </a>
                 </Link>
               </li>
+            </ul>
+          </Col>
+          <Col xs={6}>
+            <ul className="menuLateral">
               <li>
                 <Link href="/cotizacion/livechat">
                   <a>
                   <span class="lnr lnr-arrow-right"></span> KIA.COM
+                  </a>
+                </Link>
+              </li>
+              <li>
+                <Link href="/cotizacion/vr">
+                  <a>
+                  <span class="lnr lnr-arrow-right"></span> VIRTUAL REALITY
                   </a>
                 </Link>
               </li>
