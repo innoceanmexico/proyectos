54982fa3b0e243dfd20be32d2479fc3876178a5e
Axel <axosar@gmail.com>
2021-02-24 14:14:48 -0600

    logos presentacion

--- pages/presentacion/index.js
+++ pages/presentacion/index.js
@@ -13,8 +13,20 @@ export default function Proyectos() {
       {/*<NavbarPrincipal />*/}
       <div className="presentacion">
         <Container fluid={true} className="p-0 m-0 intro">
+          <div align="center">
+            <img src="/images/logo-blanco.png" className="mb-4" alt="INNOCEAN MÉXICO" title="INNOCEAN MÉXICO" />
+          </div>
           <h2>PRESENTACIÓN</h2>
           <h3>DIGITAL DEVELOPMENT</h3>
+          <hr/>
+          <div className="logos">
+            <div align="center">
+              <img src="/images/logo-kia-blanco.svg" className="mb-4" alt="INNOCEAN MÉXICO" title="INNOCEAN MÉXICO" />
+            </div>
+            <div align="center">
+              <img src="/images/logo-hyundai.svg" className="mb-4" alt="INNOCEAN MÉXICO" title="INNOCEAN MÉXICO" />
+            </div>
+          </div>
         </Container>
       </div>
     </Layout>
