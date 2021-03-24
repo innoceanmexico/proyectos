a65894dedaa2eef7a87624ac717c434ba996343d
Axel <axosar@gmail.com>
2021-03-24 12:12:09 -0600

    Login

--- pages/dashboards/index.js
+++ pages/dashboards/index.js
@@ -1,12 +1,45 @@
+import React, { useState } from 'react'
 import Layout from '../../components/layout'
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
 
 export default function Dashboards(){
+
+  const [acceso, setAcceso] = useState(false);
+
+  const usuario = (e) => {
+    e.preventDefault();
+    const password = e.target.password.value;
+    if(password === elusuario){
+      console.log(password);
+      setAcceso(true);
+    } else {
+      console.log("No es usuario");
+      setAcceso(false);
+    }
+  }
+
   return(
     <Layout>
       <Container>
+        <Row>
+          <Col>
+            <hr/>
+            <h2>LOGIN DE USUARIOS</h2>
+            <hr/>
+            <Form method="POST" onSubmit={usuario}>
+                <Form.Group controlId="formBasicPassword">
+                  <Form.Control type="password" placeholder="Contraseña" name="password" />
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                  INGRESAR
+                </Button>
+              </Form>
+          </Col>
+        </Row>
         <Row>
           <Col>
             <hr/>
