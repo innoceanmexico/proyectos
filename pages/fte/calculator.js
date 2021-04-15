1a4b22cb546f5de32b0851855567d87324e2e987
Axel <axosar@gmail.com>
2021-04-15 17:14:54 -0500

    calculadora

--- pages/fte/calculator.js
+++ pages/fte/calculator.js
@@ -19,21 +19,95 @@ function Calculator() {
               </span>
             </h2>
             <hr />
+            <h2><span className="lnr lnr-chevron-right"></span> Salarios relacionados </h2>
+            <hr/>
           </Col>
         </Row>
         <Row>
           <Col>
+            <h4>MEDIA SALARIAL</h4>
+            <p><small>240 horas mensuales promedio</small></p>
+            <hr/>
+            <Row>
+              <Col><h5>Desarrollador Java</h5></Col>
+              <Col>
+                <p>Media salarial</p>
+                <h3>$ 22,950.00<span style={{fontSize: '10px'}}> al mes</span></h3>
+                <p><small>$ 95.62 POR HORA</small></p>
+                <p>2,451 Salarios</p>
+              </Col>
+            </Row>
+            <hr/>
+            <Row>
+              <Col><h5>Programador/a</h5></Col>
+              <Col>
+                <p>Media salarial</p>
+                <h3>$ 14,753.00<span style={{fontSize: '10px'}}> al mes</span></h3>
+                <p><small>$ 61.47 POR HORA</small></p>
+                <p>1,578 Salarios</p>
+              </Col>
+            </Row>
+            <hr/>
+            <Row>
+              <Col><h5>Desarrollo</h5></Col>
+              <Col>
+                <p>Media salarial</p>
+                <h3>$ 11,303.00<span style={{fontSize: '10px'}}> al mes</span></h3>
+                <p><small>$ 47.09 POR HORA</small></p>
+                <p>1,342 Salarios</p>
+              </Col>
+            </Row>
+            <hr/>
+            <Row>
+              <Col><h5>Developer</h5></Col>
+              <Col>
+                <p>Media salarial</p>
+                <h3>$ 33,480.00<span style={{fontSize: '10px'}}> al mes</span></h3>
+                <p><small>$ 139.50 POR HORA</small></p>
+                <p>1,060 Salarios</p>
+              </Col>
+            </Row>
+            <hr/>
+            <Row>
+              <Col><h5>Soporte</h5></Col>
+              <Col>
+                <p>Media salarial</p>
+                <h3>$ 12,370.00<span style={{fontSize: '10px'}}> al mes</span></h3>
+                <p><small>$ 51.54 POR HORA</small></p>
+                <p>955 Salarios</p>
+              </Col>
+            </Row>
+            <hr/>
+            <Row>
+              <Col><h5>Desarrollador WEB</h5></Col>
+              <Col>
+                <p>Media salarial</p>
+                <h3>$ 17,656.00<span style={{fontSize: '10px'}}> al mes</span></h3>
+                <p><small>$ 73.56 POR HORA</small></p>
+                <p>647 Salarios</p>
+              </Col>
+            </Row>
+            <hr/>
+          </Col>
+          <Col>
+            <h4>CALCULADORA</h4>
+            <p><small>Calcular valor unitario</small></p>
+            <hr/>
             <Form>
-              <Form.Group controlId="formBasicEmail">
-                <Form.Label>CARGO:</Form.Label>
-                <Form.Control type="email" placeholder="Enter email" />
-                <Form.Text className="text-muted">
-                  We'll never share your email with anyone else.
-    </Form.Text>
+              <Form.Group controlId="exampleForm.ControlSelect1">
+                <Form.Label>TIPO:</Form.Label>
+                <Form.Control as="select">
+                  <option value="22950">Desarrollador Java</option>
+                  <option value="14753">Programador/a</option>
+                  <option value="11303">Desarrollo</option>
+                  <option value="33480">Developer</option>
+                  <option value="12370">Soporte</option>
+                  <option value="17656">Desarrollador WEB</option>
+                </Form.Control>
               </Form.Group>
-              <Form.Group controlId="formBasicPassword">
-                <Form.Label>HORAS TRABAJADAS:</Form.Label>
-                <Form.Control type="password" placeholder="Password" />
+              <Form.Group controlId="formBasicEmail">
+                <Form.Label>HORAS:</Form.Label>
+                <Form.Control type="number" placeholder="Ingresar horas" />
               </Form.Group>
               <Button variant="primary" type="submit">
                 CALCULAR FTE
